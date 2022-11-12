e79e69c8cd5e79b964773bd0b73bd691f3b2d05f
hassan <zare@yahoo.com>
2022-11-12 22:45:11 +0330

    Add-ValueAnderore-SingleColor-CopytoClipboard

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    try {
+      let colors = new Values(color).all(10);
+      setError(false);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
@@ -23,15 +30,24 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#f15025"
+            className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
-            Submit
+            submit
           </button>
         </form>
       </section>
-
-      <section className="color">
-        <h4>List Goes Here</h4>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
